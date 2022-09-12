5b15af7c8bbf50954248c296c8ebf3c7f735560d
Muhammad Julfikar Hyder <60023406+TheJulfikar@users.noreply.github.com>
2022-09-12 15:56:13 +0600

    Create egg.py

---
+++ egg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import random
+
+random.seed(10)
+##ab92d9fae5ee7975c7735376ec60851b
+mylist=["7","c","a","7","4","c","2","4","8","c"]
+random.shuffle(mylist)
+print(''.join(mylist))
